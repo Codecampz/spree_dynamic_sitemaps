3595489aa5b20466d2c94ee20b18b95e7f413a0f
Hendra Gunawan <hendragunz@codecampz.com>
2015-11-06 16:13:59 +0700

    small updates

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,9 +1,15 @@
 class SitemapController < Spree::BaseController
+  layout false
+
   def index
     @public_dir = root_url
+
     respond_to do |format|
+      format.html
+      format.text
+
       format.xml do
-        render :layout => false, :xml => _build_xml(@public_dir)
+        render layout: false, xml: _build_xml(@public_dir)
       end
     end
   end
@@ -16,36 +22,36 @@ class SitemapController < Spree::BaseController
       xml.instruct!  :xml, :version => "1.0", :encoding => "UTF-8"
       xml.urlset( :xmlns => "http://www.sitemaps.org/schemas/sitemap/0.9" ) {
         xml.url {
-          xml.loc public_dir
-          xml.lastmod Date.today
-          xml.changefreq 'daily'
-          xml.priority '1.0'
+          xml.loc         public_dir
+          xml.lastmod     Date.today
+          xml.changefreq  'weekly'
+          xml.priority    '1.0'
         }
+
         Spree::Taxonomy.all.each do |taxonomy|
           taxonomy.taxons.each do |taxon|
             v = _build_taxon_hash(taxon)
             xml.url {
-              xml.loc public_dir + v['link']
-              xml.lastmod v['updated'].xmlschema			  #change timestamp of last modified
-              xml.changefreq 'weekly'
-              xml.priority '0.8'
+              xml.loc         public_dir + v['link']
+              xml.lastmod     v['updated'].xmlschema        #change timestamp of last modified
+              xml.changefreq  'weekly'
+              xml.priority    '0.9'
             }
           end
         end
 
-        Spree::Product.active.includes(:variants, :taxons).find_in_batches do |batch|
-          batch.each do |product|
-            product.variants.each do |variant|
-              next variant.deleted?
-              next unless variant.manufacturer_number.present?
-              v = _build_variant_hash(product, variant)
-              xml.url {
-                xml.loc public_dir + v['link']
-                xml.lastmod v['updated'].xmlschema			  #change timestamp of last modified
-                xml.changefreq 'weekly'
-                xml.priority '0.8'
-              }
-            end
+        Spree::Product.active.includes(:variants, :taxons).each do |product|
+          next if product.deleted?
+          product.variants.each do |variant|
+            next if variant.deleted?
+            next unless variant.manufacturer_number.present
+            v = _build_variant_hash(product, variant)
+            xml.url {
+              xml.loc         public_dir + v['link']
+              xml.lastmod     v['updated'].xmlschema        #change timestamp of last modified
+              xml.changefreq  'weekly'
+              xml.priority    '0.7'
+            }
           end
         end
 
@@ -54,27 +60,27 @@ class SitemapController < Spree::BaseController
   end
 
   def _build_taxon_hash(taxon)
-    tinfo = {}
-    tinfo['name'] = taxon.name
-    tinfo['depth'] = taxon.permalink.split('/').size
-    tinfo['link'] = 't/' + taxon.permalink
-    tinfo['updated'] = taxon.updated_at
+    tinfo             = {}
+    tinfo['name']     = taxon.name
+    tinfo['depth']    = taxon.permalink.split('/').size
+    tinfo['link']     = 't/' + taxon.permalink
+    tinfo['updated']  = taxon.updated_at
     tinfo
   end
 
   def _build_product_hash(product)
-    pinfo = {}
-    pinfo['name'] = product.name
-    pinfo['link'] = 'products/' + product.permalink	# primary
-    pinfo['updated'] = product.updated_at
+    pinfo             = {}
+    pinfo['name']     = product.name
+    pinfo['link']     = 'products/' + product.permalink # primary
+    pinfo['updated']  = product.updated_at
     pinfo
   end
 
   def _build_variant_hash(product, variant)
-    vinfo = {}
-    vinfo['name'] = product.name + ' (' + variant.manufacturer_number + ')'
-    vinfo['link'] = ['skus', product.manufacturer.permalink.split('/').last, variant.manufacturer_number].join('/')
-    vinfo['updated'] = product.updated_at
+    vinfo             = {}
+    vinfo['name']     = product.name + ' (' + variant.manufacturer_number + ')'
+    vinfo['link']     = ['skus', product.manufacturer.permalink.split('/').last, variant.manufacturer_number].join('/')
+    vinfo['updated']  = product.updated_at
     vinfo
   end
 
